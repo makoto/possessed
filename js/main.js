372bd99c8b4687e3aa96c500788df4ec75267021
Makoto Inoue <inouemak@googlemail.com>
2012-09-09 14:33:17 +0100

    Skipping !!

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ d3.csv("./data/every_five_seconds.csv", function(data) {
     row.video_time_sec = parseInt(row.video_time_sec)
     return row
   })
-  
+  Data = data
   
   // Data = data
   var width = 885
@@ -32,11 +32,23 @@ d3.csv("./data/every_five_seconds.csv", function(data) {
   var groupExit = d3.transition(group.exit())
     .remove()
     
+  getAttrIdx = function(name){
+    return {
+      video_time:0,
+      video_time_sec:1,
+      game_time:2,
+      team:3,
+      narration:4,
+      player:5
+    }[name]
+  }
+    
   pop = Popcorn("#mcfc");
 
   State = {
     idx:0,
-    attr:null
+    attr:null,
+    val: null
   }
 
   var draw = function(data){
@@ -47,13 +59,13 @@ d3.csv("./data/every_five_seconds.csv", function(data) {
     var players = _.pluck(data, 'player')
     //  Making sure that blank comes first so that I can assign grey
     var unique_players = _.uniq([""].concat(players))
-    console.log('unique players', unique_players)
+    // console.log('unique players', unique_players)
     var color = d3.scale.category10().domain(unique_players).range(['grey', 'yellow', 'green', 'purple', 'pink'])
 
 
     groupEnter
       .append('rect')
-        .attr('class', function(d){return "indicator " + d.possession})
+        .attr('class', function(d){return "indicator " + d.team})
         .attr("y", 2)
         .attr("x", function(d,i){
           return l(prev(d,i).video_time_sec - d3.min(secs))
@@ -62,8 +74,17 @@ d3.csv("./data/every_five_seconds.csv", function(data) {
           var diff = d.video_time_sec - prev().video_time_sec
           return l(diff)
         })
-        .attr('height',10)
+        .attr('height',15)
         // .attr("class",  function(d){return d.possession})
+
+    groupEnter
+      .append('text')
+        .attr("y", 15)
+        .attr("x", function(d,i){
+          return l(prev(d,i).video_time_sec - d3.min(secs))
+        })
+        .text(function(d,i){return i + ''})
+
 
 
     groupEnter
@@ -72,12 +93,13 @@ d3.csv("./data/every_five_seconds.csv", function(data) {
         .on('click', function(d,i){
           State.idx = i
           State.attr = 'team'
+          State.val  = d['team']
           pop.play(d.video_time_sec)
           d3.select('#footnotediv').attr('class', null)
         })
       .append('rect')
         .attr('class', 'team')
-        .attr("y", 10)
+        .attr("y", 20)
         .attr("x", function(d,i){
           return l(prev(d,i).video_time_sec - d3.min(secs))
         })
@@ -86,7 +108,7 @@ d3.csv("./data/every_five_seconds.csv", function(data) {
           return l(diff)
         })
         .attr('height',40)
-        .attr("class",  function(d){return d.possession})
+        .attr("class",  function(d){return d.team})
 
     groupEnter.append("circle")
     .attr("cy", 28.5)
@@ -136,18 +158,68 @@ d3.csv("./data/every_five_seconds.csv", function(data) {
   //   pop.on(e, function(a){Paused = this; console.log(e, this.video.currentTime)})
   // })
   pop.on('timeupdate', function(a){
-   if (data[State.idx] && data[State.idx].video_time_sec < this.video.currentTime) {
-     var current_event = data[State.idx]
+   if (this.video.currentTime > 385) {
+     pop.pause()
+   };
+   var element = (d3.selectAll('.indicator')[0][State.idx])
+   //  Highlighting element while playing
+   
+   d3.selectAll('.indicator').attr('class', function(d){return "indicator " + d.team})
+   d3.select(element).attr('class', 'indicator g')
+
+   var current = data[State.idx]
+
+   // if (data[State.idx].video_time_sec < this.video.currentTime) {
+     console.log('timeupdate', State.idx, data[State.idx].video_time_sec < this.video.currentTime, this.video.currentTime)
+   // };
+
+   if (current && current.video_time_sec < this.video.currentTime) {
+   //   var now = State.idx
+   //   var now_event = function(){return data[now]}
+   //   // var current_event = function(){return data[State.idx]}
+   //   var next_event = function(){return data[State.idx + 1]}
+   // 
+   //   if (now_event()[State.attr] != next_event()[State.attr]) {
+   //     
+   //     while(now_event()[State.attr] != next_event()[State.attr]){
+   //       console.log('while:','now', now,'idx',State.idx,'current', current_event()[State.attr] ,'next',next_event()[State.attr])
+   //       State.idx ++
+   //       // console.log(State.idx, State.attr, current_event[State.attr] == next_event[State.attr], 'c',current_event[State.attr],'n', next_event[State.attr])
+   //     }
+   //      console.log('skipping:','now', now, 'idx',State.idx, 'next idx',State.idx + 1,  next_event()[State.attr], next_event().video_time_sec)
+   //      pop.play(current_event().video_time_sec)
+   //   }else{
+   //     State.idx++
+   //   };
+
      d3.select('#footnotediv')
-      .attr('class', current_event.possession)
-      .text(current_event.narration)
+      .attr('class', current.team)
+      .text(current.narration)
 
-     var element = (d3.selectAll('.indicator')[0][State.idx])
-     // console.log('element', element)
-     //  Highlighting element while playing
-     d3.select(element).attr('class', 'indicator g').transition().delay(5000).attr('class', "indicator " + current_event.possession)
+     // State.idx++
+     var next = data[State.idx + 1]
+   
+     if (State.val != next[State.attr]) {
+       console.log('mismatch', current.video_time_sec)
+       while(next && State.val != next[State.attr]){
+         console.log('Incrementing ', State.idx, State.val, current[State.attr], next[State.attr])
+         State.idx++
+         var next = data[State.idx]
+       }
+       console.log('Skipping ', State.idx)
+       if (next) {
+         pop.play(next.video_time_sec)
+       }else{
+         pop.pause()
+       };
 
-    State.idx++
+     
+     }else{
+       console.log('going to next ', State.idx++, State.val, current[State.attr], next[State.attr])
+     };
+   
    }
+   
+   
   })
 })
